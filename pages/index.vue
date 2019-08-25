b10de3793ae6e804882b44cf54c8335646980dae
Takashi Iwamoto <hello@iwamot.com>
2019-08-25 23:06:53 +0900

    Update title

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@ export default {
 
   head() {
     return {
-      title: 'didit：情報処理技術者試験の合格状況をカッコよく表現',
+      title: 'didit：情報処理技術者試験の合格状況をカッコよく表示',
       meta: [
         {
           hid: 'description',
